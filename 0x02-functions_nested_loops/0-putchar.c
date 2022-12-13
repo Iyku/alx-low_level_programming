396b4d03286968817b3156e36e5cab52a2687c13
Iyku <iugwu86@gmail.com>
2022-12-13 15:14:09 -0800

    now

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- 
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
+ * main - prints _putchar and a new line
+ * Return: Always 0
  */
-
 int main(void)
-
 {
 
-		char *sh = "Holberton";
+	_putcha('_');
+	_putcha('p');
+	_putcha('u');
+	_putcha('t');
+	_putcha('c');
+	_putcha('h');
+	_putcha('a');
+	_putcha('r');
+	_putcha('\n');
 
-
-
-		while (*sh)
-
-		{
-
-			_putchar(*sh);
-
-			sh++;
-
-		}
-
-		_putchar('\n');
-
-
-
-		return (0);
+	return (0);
 
 }
